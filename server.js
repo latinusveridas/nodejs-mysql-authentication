817b1198a5471d14849cd0589510c5678a872305
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2018-10-08 12:04:46 +0200

    servers cleaned

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var cors = require('cors');
 var bodyParser = require("body-parser");
 var app = express();
 var port = process.env.PORT || 3000;
-var database = require('../Database/database');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -14,20 +13,6 @@ app.use(bodyParser.urlencoded({
 var Users = require('./Routes/Users');
 
 app.use('/users',Users);
-
-app.get('/testdb', function (req, res) {
-
-    database.connection.getConnection(function (err, connection) {
-        if (err) {
-            console.log("DATABASE PROBLEM");
-        } else {
-            console.log("Connection Successful !!!")
-
-            connection.release();
-        }
-    });
-});
-
 
 app.listen(port,function(){
     console.log("Server is running on port: "+port);

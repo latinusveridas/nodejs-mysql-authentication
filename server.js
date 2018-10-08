89461afa06f0aafd528dcff787f01254fbf56e74
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2018-10-08 14:46:41 +0200

    sql amended

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require("body-parser");
 var app = express();
+var mysql = require("mysql");
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -10,9 +11,22 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+
+///////
 var Users = require('./Routes/Users');
 
 app.use('/users', Users);
+
+var connection = mysql.createPool({
+    connectionLimit: 100,
+    host: 'localhost',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: 3306,
+    debug: false,
+    multipleStatements: true
+});
 
 connection.getConnection(function (err, connection) {
     if (err) {

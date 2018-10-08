8ae1cffdc7930a2856c206f219ca0f71131ceba8
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2018-10-08 11:48:39 +0200

    added testdb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var cors = require('cors');
 var bodyParser = require("body-parser");
 var app = express();
 var port = process.env.PORT || 3000;
+var database = require('../Database/database');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,6 +14,22 @@ app.use(bodyParser.urlencoded({
 var Users = require('./Routes/Users');
 
 app.use('/users',Users);
+
+users.get('/testdb', function (req, res) {
+
+    database.connection.getConnection(function (err, connection) {
+        if (err) {
+            appData["error"] = 1;
+            appData["data"] = "Internal Server Error";
+            res.status(500).json(appData);
+        } else {
+            console.log("Connection Successful !!!")
+
+            connection.release();
+        }
+    });
+});
+
 
 app.listen(port,function(){
     console.log("Server is running on port: "+port);
